e96cfe81d82e76409797ae67ce16b6db6d6fc0d1
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2021-09-10 12:01:53 +0900

    version 1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='macpywm',
-    version='1.1',
+    version='1.2',
     author='Hiroyuki Ohsaki',
     author_email='ohsaki@lsnl.jp',
     description='A simple but extensible window manager for macOS',
